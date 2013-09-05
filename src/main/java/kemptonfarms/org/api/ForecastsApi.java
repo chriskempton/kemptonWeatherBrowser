39e1076c4e1d32f38f354736acef0be95d610fda
Chris Kempton <chris.kempton@ithaka.org>
2013-09-05 10:05:35 -0400

    Update to generated ForecastsApi.  Removes HTML and XML requestMappings, as those outputs are now handled by a different controller

--- src/main/java/kemptonfarms/org/api/ForecastsApi.java
+++ src/main/java/kemptonfarms/org/api/ForecastsApi.java
@@ -36,30 +36,6 @@ public class ForecastsApi {
   }
   
   /**
-   * Run a GET with ZIP code in the path, get output as HTML page
-   *
-   * 
-   * 
-   * @return String response
-   */
-  
-  @RequestMapping(value="/htmltable/{input}", method={RequestMethod.GET})
-  @ResponseBody
-  @ApiOperation(value = "Run a GET with ZIP code in the path, get output as HTML page", notes = "", responseClass = "String")
-  @ApiErrors(value = { @ApiError(code = 400, reason = "Bad Request"),@ApiError(code = 401, reason = "Unauthorized"),@ApiError(code = 403, reason = "Forbidden"),@ApiError(code = 404, reason = "Not Found"),@ApiError(code = 405, reason = "Method Not Allowed"),@ApiError(code = 408, reason = "Request Timeout"),@ApiError(code = 415, reason = "Unsupported Media Type"),@ApiError(code = 424, reason = "Method Failure"),@ApiError(code = 500, reason = "Internal Server Error"),@ApiError(code = 501, reason = "Not Implemented"),@ApiError(code = 502, reason = "Bad Gateway"),@ApiError(code = 503, reason = "Service Unavailable"),@ApiError(code = 504, reason = "Gateway Timeout")})
-  public String htmltable(
-    @ApiParam(value = "ZIP code"
-      ,required=true)
-    @PathVariable(value = "input")String input
-    ,HttpServletRequest request, HttpServletResponse response
-  ) throws NotFoundException {
-      
-      return worker.htmltable(
-        input
-        ,request, response);
-      }
-
-  /**
    * Run a GET with ZIP code in the path, get output as JSON array
    *
    * 
@@ -79,30 +55,6 @@ public class ForecastsApi {
   ) throws NotFoundException {
       
       return worker.json(
-        input
-        ,request, response);
-      }
-
-  /**
-   * Run a GET with ZIP code in the path, get output as XML document
-   *
-   * 
-   * 
-   * @return String response
-   */
-  
-  @RequestMapping(value="/xml/{input}", method={RequestMethod.GET})
-  @ResponseBody
-  @ApiOperation(value = "Run a GET with ZIP code in the path, get output as XML document", notes = "", responseClass = "String")
-  @ApiErrors(value = { @ApiError(code = 400, reason = "Bad Request"),@ApiError(code = 401, reason = "Unauthorized"),@ApiError(code = 403, reason = "Forbidden"),@ApiError(code = 404, reason = "Not Found"),@ApiError(code = 405, reason = "Method Not Allowed"),@ApiError(code = 408, reason = "Request Timeout"),@ApiError(code = 415, reason = "Unsupported Media Type"),@ApiError(code = 424, reason = "Method Failure"),@ApiError(code = 500, reason = "Internal Server Error"),@ApiError(code = 501, reason = "Not Implemented"),@ApiError(code = 502, reason = "Bad Gateway"),@ApiError(code = 503, reason = "Service Unavailable"),@ApiError(code = 504, reason = "Gateway Timeout")})
-  public String xml(
-    @ApiParam(value = "ZIP code"
-      ,required=true)
-    @PathVariable(value = "input")String input
-    ,HttpServletRequest request, HttpServletResponse response
-  ) throws NotFoundException {
-      
-      return worker.xml(
         input
         ,request, response);
       }
